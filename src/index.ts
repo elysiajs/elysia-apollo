5fd726ee6c7c3375e9f7488982828c5cbcc74b84
Vincent Vella <vincevella0@gmail.com>
2023-10-31 01:41:48 -0400

    fix: update formatting and type-only imports

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import { t } from 'elysia'
 import type { Context, Elysia } from 'elysia'
-
-import {
-    ApolloServer,
-    BaseContext,
-    ContextThunk,
-    type ApolloServerOptions
-} from '@apollo/server'
+import { ApolloServer, BaseContext } from '@apollo/server'
 import {
     ApolloServerPluginLandingPageLocalDefault,
     ApolloServerPluginLandingPageProductionDefault
 } from '@apollo/server/plugin/landingPage/default'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground'
-import { type StartStandaloneServerOptions } from '@apollo/server/standalone'
+
+import type { StartStandaloneServerOptions } from '@apollo/server/standalone'
+import type { ApolloServerOptions } from '@apollo/server'
 
 export interface ServerRegistration<Path extends string = '/graphql'>
     extends Omit<StartStandaloneServerOptions<any>, 'context'> {
@@ -36,19 +32,19 @@ export class ElysiaApolloServer<
     public async createHandler<Path extends string = '/graphql'>({
         path = '/graphql' as Path,
         enablePlayground,
-        context = async () => {}
+        context = async () => { }
     }: ServerRegistration<Path>) {
         const landing = enablePlayground
             ? ApolloServerPluginLandingPageGraphQLPlayground({
-                  endpoint: path
-              })
+                endpoint: path
+            })
             : process.env.ENV === 'production'
-            ? ApolloServerPluginLandingPageProductionDefault({
-                  footer: false
-              })
-            : ApolloServerPluginLandingPageLocalDefault({
-                  footer: false
-              })
+                ? ApolloServerPluginLandingPageProductionDefault({
+                    footer: false
+                })
+                : ApolloServerPluginLandingPageLocalDefault({
+                    footer: false
+                })
 
         await this.start()
 
