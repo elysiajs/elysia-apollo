2b50770f4a607f547167e9a115e3eecb5c98b464
saltyaom <saltyaom@gmail.com>
2024-07-16 18:57:22 +0700

    :broom: chore: bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+
+# 1.1.0 - 16 Jul 2024
+Change:
+- Add support for Elysia 1.1
+
+
+# 1.1.0-rc.0 - 12 Jul 2024
+Change:
+- Add support for Elysia 1.1
+
 # 1.0.3 - 30 Mar 2024
 Feature:
 - [#8](https://github.com/elysiajs/elysia-apollo/pull/8) add TContext for Apollo type
--- src/index.ts
+++ src/index.ts
@@ -1,146 +1,149 @@
-import { t } from 'elysia'
-import type { Context, Elysia, Handler } from 'elysia'
+import { Elysia, t } from 'elysia'
 
 import {
-    ApolloServer,
-    BaseContext,
-    ContextThunk,
-    type ApolloServerOptions
+	ApolloServer,
+	BaseContext,
+	type ApolloServerOptions
 } from '@apollo/server'
 import {
-    ApolloServerPluginLandingPageLocalDefault,
-    ApolloServerPluginLandingPageProductionDefault
+	ApolloServerPluginLandingPageLocalDefault,
+	ApolloServerPluginLandingPageProductionDefault
 } from '@apollo/server/plugin/landingPage/default'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground'
 import { type StartStandaloneServerOptions } from '@apollo/server/standalone'
 
-export interface ServerRegistration<Path extends string = '/graphql', TContext extends BaseContext = BaseContext>
-    extends Omit<StartStandaloneServerOptions<any>, 'context'> {
-    path?: Path
-    enablePlayground: boolean
-    context?: (context: TContext) => Promise<TContext>
+export interface ServerRegistration<
+	Path extends string = '/graphql',
+	TContext extends BaseContext = BaseContext
+> extends Omit<StartStandaloneServerOptions<any>, 'context'> {
+	path?: Path
+	enablePlayground: boolean
+	context?: (context: TContext) => Promise<TContext>
 }
 
 export type ElysiaApolloConfig<
-    Path extends string = '/graphql',
-    TContext extends BaseContext = BaseContext
+	Path extends string = '/graphql',
+	TContext extends BaseContext = BaseContext
 > = ApolloServerOptions<TContext> &
-    Omit<ServerRegistration<Path, TContext>, 'enablePlayground'> &
-    Partial<Pick<ServerRegistration, 'enablePlayground'>>
+	Omit<ServerRegistration<Path, TContext>, 'enablePlayground'> &
+	Partial<Pick<ServerRegistration, 'enablePlayground'>>
 
 const getQueryString = (url: string) => url.slice(url.indexOf('?', 11) + 1)
 
 export class ElysiaApolloServer<
-    Context extends BaseContext = BaseContext
+	Context extends BaseContext = BaseContext
 > extends ApolloServer<Context> {
-    public async createHandler<Path extends string = '/graphql'>({
-        path = '/graphql' as Path,
-        enablePlayground,
-        context = async () => ({} as any)
-    }: ServerRegistration<Path, Context>) {
-        const landing = enablePlayground
-            ? ApolloServerPluginLandingPageGraphQLPlayground({
-                  endpoint: path
-              })
-            : process.env.ENV === 'production'
-            ? ApolloServerPluginLandingPageProductionDefault({
-                  footer: false
-              })
-            : ApolloServerPluginLandingPageLocalDefault({
-                  footer: false
-              })
+	public async createHandler<Path extends string = '/graphql'>({
+		path = '/graphql' as Path,
+		enablePlayground,
+		context: apolloContext = async () => ({}) as any
+	}: ServerRegistration<Path, Context>) {
+		const landing = enablePlayground
+			? ApolloServerPluginLandingPageGraphQLPlayground({
+					endpoint: path
+				})
+			: process.env.ENV === 'production'
+				? ApolloServerPluginLandingPageProductionDefault({
+						footer: false
+					})
+				: ApolloServerPluginLandingPageLocalDefault({
+						footer: false
+					})
 
-        await this.start()
+		await this.start()
 
-        // @ts-ignore
-        const landingPage = await landing!.serverWillStart!({}).then((r) =>
-            r?.renderLandingPage
-                ? r.renderLandingPage().then((r) => r.html)
-                : null
-        )
+		// @ts-ignore
+		const landingPage = await landing!.serverWillStart!({}).then((r) =>
+			r?.renderLandingPage
+				? r.renderLandingPage().then((r) => r.html)
+				: null
+		)
 
-        return (app: Elysia) => {
-            if (landingPage)
-                app.get(path, () => {
-                    return new Response(landingPage, {
-                        headers: {
-                            'Content-Type': 'text/html'
-                        }
-                    })
-                })
+		const executeHTTPGraphQLRequest =
+			this.executeHTTPGraphQLRequest.bind(this)
 
-            return app.post(
-                path,
-                (c) =>
-                    this.executeHTTPGraphQLRequest({
-                        httpGraphQLRequest: {
-                            method: c.request.method,
-                            body: c.body,
-                            search: getQueryString(c.request.url),
-                            request: c.request,
-                            // @ts-ignore
-                            headers: c.request.headers
-                        },
-                        // @ts-ignore
-                        context: () => context(c)
-                    })
-                        .then((res) => {
-                            if (res.body.kind === 'complete')
-                                return new Response(res.body.string, {
-                                    status: res.status ?? 200,
-                                    // @ts-ignore
-                                    headers: res.headers
-                                })
+		const app = new Elysia()
 
-                            return new Response('')
-                        })
-                        .catch((error) => {
-                            if (error instanceof Error) throw error
+		if (landingPage)
+			app.get(
+				path,
+				new Response(landingPage as string, {
+					headers: {
+						'Content-Type': 'text/html'
+					}
+				})
+			)
 
-                            return new Response(error.message, {
-                                status: error.statusCode
-                            })
-                        }),
-                {
-                    body: t.Object(
-                        {
-                            operationName: t.Optional(
-                                t.Union([t.String(), t.Null()])
-                            ),
-                            query: t.String(),
-                            variables: t.Optional(
-                                t.Object(
-                                    {},
-                                    {
-                                        additionalProperties: true
-                                    }
-                                )
-                            )
-                        },
-                        {
-                            additionalProperties: true
-                        }
-                    )
-                }
-            )
-        }
-    }
+		return app.post(
+			path,
+			async function handleGraph(context) {
+				const {
+					body,
+					request,
+					request: { method, url, headers },
+					set
+				} = context
+
+				const res = await executeHTTPGraphQLRequest({
+					httpGraphQLRequest: {
+						method,
+						body,
+						search: getQueryString(url),
+						request,
+						// @ts-ignore
+						headers
+					},
+					// @ts-ignore
+					context: () => apolloContext(context)
+				}).catch((x) => x)
+
+				if (res.body.kind !== 'complete') return ''
+
+				return new Response(res.body.string, {
+					status: res.status ?? 200,
+					// @ts-ignore
+					headers: res.headers
+				})
+			},
+			{
+				body: t.Object(
+					{
+						operationName: t.Optional(
+							t.Union([t.String(), t.Null()])
+						),
+						query: t.String(),
+						variables: t.Optional(
+							t.Object(
+								{},
+								{
+									additionalProperties: true
+								}
+							)
+						)
+					},
+					{
+						additionalProperties: true
+					}
+				)
+			}
+		)
+	}
 }
 
 export const apollo = async <
-    Path extends string = '/graphql',
-    TContext extends BaseContext = BaseContext,
+	Path extends string = '/graphql',
+	TContext extends BaseContext = BaseContext
 >({
-    path,
-    enablePlayground = process.env.ENV !== 'production',
-    context,
-    ...config
+	path,
+	enablePlayground = process.env.ENV !== 'production',
+	context,
+	...config
 }: ElysiaApolloConfig<Path, TContext>) =>
-    new ElysiaApolloServer<TContext>(config).createHandler<Path>({
-        context,
-        path,
-        enablePlayground
-    })
+	new ElysiaApolloServer<TContext>(config).createHandler<Path>({
+		context,
+		path,
+		enablePlayground
+	})
 
 export { gql } from 'graphql-tag'
 
